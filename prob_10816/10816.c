e2ea03f0576ea019503003c68d7977eccdbfbc93
gerzees <rudwls0511@pusan.ac.kr>
2021-12-29 20:38:59 +0900

    10816 is done, but is not submitted.

--- prob_10816/10816.c
+++ prob_10816/10816.c
@@ -1,27 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct _card_
+enum
 {
-    int num;
-    int cnt;
-} Card;
+    MAX_CNT = 500000
+};
+int compare_int(const void *arg1, const void *arg2)
+{
+    const int i1 = *(const int *)arg1;
+    const int i2 = *(const int *)arg2;
+    return (i1 > i2) - (i1 < i2);
+}
+int search_position(const int target, const int *sorted_arr, const int begin, const int end)
+{
+    int center = (begin + end) / 2;
+    if (end - begin < 1)
+        return -1; //no element: not found target
+    if (sorted_arr[center] < target)
+        return search_position(target, sorted_arr, center + 1, end);
+    else if (target < sorted_arr[center])
+        return search_position(target, sorted_arr, begin, center);
+    else //target found
+        return center;
+}
+int count_int(const int target, const int *sorted_arr, const int arr_len)
+{
+    int cnt = 0;
+    int pos = search_position(target, sorted_arr, 0, arr_len);
+    if (pos < 0)
+        return cnt;
+    ++cnt; //at least, there is one card
 
+    for (size_t i = pos - 1; 0 <= i && !(target != sorted_arr[i]); --i, ++cnt)
+        ; //check left side from pos
+    for (size_t i = pos + 1; i < arr_len && !(target != sorted_arr[i]); ++i, ++cnt)
+        ; //check right side from pos
+    return cnt;
+}
 int main(void)
 {
     int N, M;
-    int kind_card;
-    Card cards[]
+    // static int cards[MAX_CNT];
     scanf("%d", &N);
+    int cards[N];
     for (size_t i = 0; i < N; i++)
-    {
-        /* code */
-    }
+        scanf("%d", &cards[i]);
+    qsort(cards, N, sizeof(int), compare_int);
     scanf("%d", &M);
     for (size_t i = 0; i < M; i++)
     {
-        /* code */
+        int target;
+        scanf("%d", &target);
+        printf("%d ", count_int(target, cards, N));
     }
-    
-    
     return 0;
 }
