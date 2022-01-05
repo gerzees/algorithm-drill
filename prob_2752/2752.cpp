7d8de747f0ac82c2639ef6803f39bdc312441b32
gerzees <rudwls0511@pusan.ac.kr>
2022-01-05 15:50:20 +0900

    index wrong,fail --> fix,submit

--- prob_2752/2752.cpp
+++ prob_2752/2752.cpp
@@ -10,7 +10,7 @@ int main(void)
     if (n[0] > n[1])
         swap<int>(n[0], n[1]);
     if (n[0] > n[2])
-        swap<int>(n[0], n[1]);
+        swap<int>(n[0], n[2]);
     if (n[1] > n[2])
         swap<int>(n[1], n[2]);
     cout << n[0] << ' ' << n[1] << ' ' << n[2];
