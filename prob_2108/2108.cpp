b0cb1195cc3b8bab82d84f377bdc61875e61ced0
gerzees <rudwls0511@pusan.ac.kr>
2022-01-19 22:16:25 +0900

    fix,success

--- prob_2108/2108.cpp
+++ prob_2108/2108.cpp
@@ -1,5 +1,5 @@
 //2108: 통계학
-//Wrong
+//fix
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -7,17 +7,18 @@ using namespace std;
 int main(void)
 {
     cin.tie(0)->sync_with_stdio(false);
-    uint16_t cnt[8001] = {
+    //cnt range 0~500,000 but uint16_t max 65535
+    uint32_t cnt[8001] = {
         0,
     };
     //index=num+4000
     //index 0       4000    4001    8000
     //num   -4000   0       1       4000
-    uint cntNum, cntMid, freq = 0;
+    int cntNum, freq = 0;
     int min = 4000, max = -4000, sum = 0, mostFreqNum, mid, ave;
     bool isOnlyMostFreq = false, foundMid = false;
     cin >> cntNum;
-    for (size_t i = 0; i < cntNum; i++)
+    for (int i = 0; i < cntNum; i++)
     {
         int num;
         cin >> num;
@@ -28,12 +29,12 @@ int main(void)
             max = num;
         sum += num; //acc sum;
     }
-    for (int i = 0, cntMid = 0; i < 8001; i++)
+    for (int i = 0, cntChecked = 0; i < 8001; i++)
     {
         if (!foundMid) //find middle value
         {
-            cntMid += cnt[i];
-            if (cntMid >= cntNum / 2 + 1)
+            cntChecked += cnt[i];
+            if (cntChecked >= cntNum / 2 + 1)
             {
                 mid = i - 4000;
                 foundMid = true;
@@ -51,13 +52,16 @@ int main(void)
             mostFreqNum = i - 4000;
         }
     }
-    int rounding = sum * 10 / int(cntNum) % 10;
+    // sum can be 2,000,000,000 and int maximum is 2,147,483,647
+    // sum*10 --> overflow!!
+    // changing 10 to 10ll works
+    int rounding = sum * 10ll / int(cntNum) % 10;
     ave = sum / int(cntNum);
     if (rounding >= 5)
         ++ave;
     else if (rounding <= -5)
         --ave;
-    // ave = round(sum / double(cntNum));
+    // ave = round(sum / double(cntNum));// works
     cout
         << ave << '\n'
         << mid << '\n'
