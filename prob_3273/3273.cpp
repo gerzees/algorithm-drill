bd4a324a71b09af65a44d74a0dc856504e57212d
gerzees <rudwls0511@pusan.ac.kr>
2022-01-25 10:24:28 +0900

    success

--- prob_3273/3273.cpp
+++ prob_3273/3273.cpp
@@ -1,28 +1,27 @@
 //3273: 두 수의 합
-// wrong
+// fix
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main(void)
 {
-    static int cnt[1000001] = {
-        0,
-    };
+    static bool isInput[1000001]; // not explicitly initialized-->zero-initialized
     int size;
     cin >> size;
     for (size_t i = 0; i < size; i++)
     {
         int num;
         cin >> num;
-        ++cnt[num];
+        isInput[num] = true;
     }
     int X, cntPair = 0;
     cin >> X;
-    for (size_t num = 1; num < 1000001; num++)
-        if (cnt[num] > 0 && 0 <= X - int(num) && X - int(num) <= 1000000)
-            if (cnt[X - num])
-                ++cntPair, --cnt[num], --cnt[X - num];
+    for (size_t num = 1; num < X / 2 + 1; num++)
+        if (2 * num != X) //X=a_i+a_j, i!=j
+            if (X - num < 1000001)//check outOfBounds
+                if (isInput[num] && isInput[X - num])
+                    ++cntPair;
     cout << cntPair;
     return 0;
 }
