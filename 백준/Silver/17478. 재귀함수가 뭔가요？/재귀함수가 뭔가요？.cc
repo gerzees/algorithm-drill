f25fe8f49cf447e739bcf23aab57fa35a42ec74b
gerzees <49828022+gerzees@users.noreply.github.com>
2023-09-18 11:36:45 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.cc
@@ -0,0 +1,57 @@
+// s 10:57
+// e 11:34
+#include <bits/stdc++.h>
+using namespace std;
+const string INTRO = "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n";
+const string QUESTION = "\"재귀함수가 뭔가요?\"\n";
+const string RECURSIVE_ANSWER_LINES[3] = { "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n",
+"마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n",
+"그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n" };
+const string RECURSIVE_FOLLOWING_LINE = "라고 답변하였지.\n";
+const string ANSWER = "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
+const string DEPTH_SYMBOL = "____";
+
+void printDepthSymbol(int depth)
+{
+	for (int i = 0;i < depth;++i) {
+		cout << DEPTH_SYMBOL;
+	}
+}
+void askRecursive(int curDepth, int goalDepth)
+{
+
+	printDepthSymbol(curDepth);
+	cout << QUESTION;
+
+	if (curDepth == goalDepth) {
+		printDepthSymbol(curDepth);
+		cout << ANSWER;
+	}
+	else {
+
+		for (int i = 0;i < 3;++i) {
+			printDepthSymbol(curDepth);
+			cout << RECURSIVE_ANSWER_LINES[i];
+		}
+		askRecursive(curDepth + 1, goalDepth);
+	}
+
+	printDepthSymbol(curDepth);
+	cout << RECURSIVE_FOLLOWING_LINE;
+
+	return;
+}
+
+void chatbot(int n)
+{
+	cout << INTRO;
+	askRecursive(0, n);
+}
+
+int main(void)
+{
+	int n;
+	cin >> n;
+	chatbot(n);
+	return 0;
+}
