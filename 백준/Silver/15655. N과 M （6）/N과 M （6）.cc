cc835765f53e463115b45bbdfb42135c85764e98
gerzees <49828022+gerzees@users.noreply.github.com>
2023-09-21 14:47:46 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -1,5 +1,5 @@
 //s 14:24
-//e 14:40
+//e 14:47
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -7,23 +7,6 @@ using namespace std;
 int N, M;
 int num[8];
 int arr[8];
-
-void selectRecursive(int index, int* ptrNum)
-{
-	if (index == M) {
-		for (int i = 0; i < M; ++i) {
-			cout << arr[i] << ' ';
-		}
-		cout << '\n';
-
-		return;
-	}
-
-	for (int* it = ptrNum; it != num + N; ++it) {
-		arr[index] = *it;
-		selectRecursive(index + 1, it + 1);
-	}
-}
 
 int main(void)
 {
@@ -34,7 +17,21 @@ int main(void)
 	}
 
 	sort(num, num + N);
-	selectRecursive(0, num);
+
+	for (int i = 0; i < N; ++i) {
+		arr[i] = i < M ? 0 : 1;
+	}
+
+	do {
+		
+		for (int i = 0; i < N; ++i) {
+			if (arr[i] == 0) {
+				cout << num[i] << ' ';
+			}
+		}
+		cout << '\n';
+
+	} while (next_permutation(arr, arr + N));
 
 	return 0;
 }
