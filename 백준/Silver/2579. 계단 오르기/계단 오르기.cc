e5a81e92d3503e629e5b72db18da48a0d8181739
gerzees <49828022+gerzees@users.noreply.github.com>
2023-11-10 15:10:31 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,39 +1,54 @@
 //s 13:38
-//e 14:13
+//e 15:10
 //배열,점화식,초기조건
-//int d1[301] d1[x] 직전 계단 밟은 경우 x번째 계단에서 얻을 수 있는 최댓값(전전 계단은 밟을 수 없다)
-//int d2[301] d2[x] 직전 계단 안밟은 경우 x번째 계단에서 얻을 수 있는 최댓값(전전 계단은 밟아야만 한다)
-// 직전에 안 밟은거 최대값d2, 직전에 밟은 거 최대값d1
-//d1[1] = step[1], d2[1] = step[1]
-//d1[2] = step[2]+step[1], d2[2] = step[2]
-//d1[3] = step[3]+d2[2], d2[3] = step[3]+max(d1[1],d2[1])
-//d1[4] = step[4]+d2[3], d2[4] = step[4]+max(d1[2],d2[2])
+//d[i]= i번째 계단까지 올라섰을 때 밟지 않을 계단의 합의 최댓값, i번째 계단은 반드시 밟은 계단으로 선택해야함
+// 뛰어 넘는 것 최대 1개
+// d[1] = step[1], d[2] = step[1]+step[2], d[3] = max(s[1],s[2])+step[3]
+// d[4] = max(d[1],d[2])+step[4],
+//  d[5] = max(d[2],d[3])+step[5]
+//d[i] = max(d[i-2],d[i-3]+step[i-1])+step[i]
 
 
 #include<bits/stdc++.h>
 
 using namespace std;
 
-int d1[301];
-int d2[301];
-int steps[301];
+int d[301];
+int step[301];
 int main(void)
 {
-	int cnt;
-	cin >> cnt;
-	for (int i = 1; i <= cnt; ++i) {
-		cin >> steps[i];
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; ++i) {
+		cin >> step[i];
 	}
 
-	d1[1] = d2[1] = steps[1];
-	d1[2] = d2[1] + steps[2];
-	d2[2] = 0 + steps[2];
-	for (int i = 3; i <= cnt;++i) {
-		d1[i] = d2[i - 1] + steps[i];
-		d2[i] = max(d1[i - 2], d2[i - 2]) + steps[i];
+	if (n == 1) {
+		cout << step[1];
+
+		return 0;
 	}
 
-	cout << max(d1[cnt], d2[cnt]);
+	if (n == 2) {
+		cout << step[1] + step[2];
+
+		return 0;
+	}
+
+	if (n == 3) {
+		cout << max(step[1], step[2]) + step[3];
+
+		return 0;
+	}
+
+	d[1] = step[1];
+	d[2] = step[1] + step[2];
+	d[3] = max(step[1], step[2]) + step[3];
+	for (int i = 4; i <= n; ++i) {
+		d[i] = max(d[i - 2], d[i - 3] + step[i - 1]) + step[i];
+	}
+
+	cout << d[n];
 
 	return 0;
 }
