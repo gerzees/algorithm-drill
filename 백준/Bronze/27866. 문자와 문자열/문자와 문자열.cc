26832abe6a7edd7ecf5030b57ca13db58179cb50
gerzees <49828022+gerzees@users.noreply.github.com>
2023-12-31 15:40:33 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+// 15:38, 15:40
+#include<bits/stdc++.h>
+using namespace std;
+
+string S;
+int i;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> S >> i;
+  cout << S[i - 1];
+}
