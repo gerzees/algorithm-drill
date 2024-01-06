f795d30207f7f4e15796f3cf5da04afcf3950fee
gerzees <49828022+gerzees@users.noreply.github.com>
2024-01-06 11:21:19 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,111 +1,63 @@
-#include <iostream>
-#include <vector>
-#include <list>
-#include <stack>
-#include <queue>
-#include <algorithm>
-#include <iterator>
-
+//11:03, 11:21
+#include<bits/stdc++.h>
 using namespace std;
 
-class Graph {
-    vector<list<int>> vertice;
-  public:
-    Graph(const int numOfVertex, const int numOfEdge) : vertice(numOfVertex+1) {
-        for(int i=0; i<numOfEdge; ++i) {
-            int vertex1, vertex2;
-            cin>> vertex1 >> vertex2;
-            vertice[vertex1].push_back(vertex2);
-            vertice[vertex2].push_back(vertex1);
-        }
-        for(int i=1; i<=numOfVertex; ++i) {
-            vertice[i].sort();
-        }
-    }
-    vector<int> DFS(const int beginNumber = 0);
-    vector<int> BFS(const int beginNumber = 0);
-    friend istream& operator>>(istream& is, Graph& graph);
-};
+vector<int> adj[1001];
+vector<bool> vis(1001, false);
+int N, M, V, u, v;
 
-vector<int> Graph::DFS(const int beginVertex) {
-    vector<int> visitSequence;
-    stack<int> visitedStack;
-    bool isVisitedAt[vertice.size()];
-    for(int i=0; i<vertice.size(); ++i)
-        isVisitedAt[i] = false;
-    int current = beginVertex;
-    visitSequence.push_back(current);
-    isVisitedAt[current] = true;
-    bool isDone = false;
-    while(!isDone) {
-        int previous = current;
-        for(list<int>::iterator i=vertice[current].begin(); i!=vertice[current].end(); ++i) {
-            int connectedVertex = *i;
-            if(!isVisitedAt[connectedVertex]) {
-                current = connectedVertex;
-                visitedStack.push(previous);
-                visitSequence.push_back(current);
-                isVisitedAt[current] = true;
-                break;
-            }
-        }
-        bool noMoreDepth = previous == current;
-        if(noMoreDepth&&!visitedStack.empty()) {
-            current = visitedStack.top();
-            visitedStack.pop();
-        } else if(noMoreDepth) {
-            isDone = true;
-        }
-    }
-    return visitSequence;
+void dfs(int cur) {
+  vis[cur] = true;
+
+  cout << cur << ' ';
+
+  for (int nxt : adj[cur]) {
+    if (vis[nxt]) continue;
+
+    dfs(nxt);
+  }
 }
 
-vector<int> Graph::BFS(const int beginVertex) {
-    vector<int> visitSequence;
-    queue<int> visitedQueue;
-    bool isVisitedAt[vertice.size()];
-    for(int i=0; i<vertice.size(); ++i)
-        isVisitedAt[i] = false;
-    int current = beginVertex;
-    visitSequence.push_back(current);
-    isVisitedAt[current] = true;
-    bool isDone = false;
-    while(!isDone) {
-        for(list<int>::iterator i=vertice[current].begin(); i!=vertice[current].end(); ++i) {
-            int connectedVertex = *i;
-            if(!isVisitedAt[connectedVertex]) {
-                visitSequence.push_back(connectedVertex);
-                isVisitedAt[connectedVertex] = true;
-                visitedQueue.push(connectedVertex);
-            }
-        }
-        if(visitedQueue.empty()) {
-            isDone = true;
-        } else {
-            current = visitedQueue.front();
-            visitedQueue.pop();
-        }
+void bfs() {
+  queue<int> q;
+  q.push(V);
+  vis[V] = true;
+
+  while (!q.empty()) {
+    int cur = q.front();
+    q.pop();
+
+    cout << cur << ' ';
+
+    for (int nxt : adj[cur]) {
+      if (vis[nxt]) continue;
+
+      q.push(nxt);
+      vis[nxt] = true;
     }
-    return visitSequence;
+  }
 }
 
-istream& operator>>(istream& is, Graph& graph) {
-    int vertex1, vertex2;
-    is>> vertex1 >> vertex2;
-    graph.vertice[vertex1].push_back(vertex2);
-    graph.vertice[vertex2].push_back(vertex1);
-    return is;
-}
 int main(void) {
-    int numOfVertex, numOfEdge, beginVertex;
-    cin >> numOfVertex >> numOfEdge >>beginVertex;
-    Graph graph(numOfVertex, numOfEdge);
-    vector<int> resultOfDFS(graph.DFS(beginVertex));
-    vector<int> resultOfBFS(graph.BFS(beginVertex));
-    for(int vertex:resultOfDFS)
-        cout << vertex << ' ';
-    cout << endl;
-    for(int vertex:resultOfBFS)
-        cout << vertex << ' ';
-    return 0;
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N >> M >> V;
+
+  while (M--) {
+    cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+
+  for (int i = 1;i <= N;++i) {
+    sort(adj[i].begin(), adj[i].end());
+  }
+
+  dfs(V);
+
+  fill(vis.begin(), vis.end(), false);
+  cout << '\n';
+
+  bfs();
 }
