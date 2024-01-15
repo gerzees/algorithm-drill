6e1fd3b4d95dc1c43c2dc947387217e9a23b9d2e
gerzees <49828022+gerzees@users.noreply.github.com>
2024-01-15 13:34:43 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.cc
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -1,30 +1,77 @@
-// 10:30, 10,:47
+// 13:05,13:25
 #include<bits/stdc++.h>
 using namespace std;
 
 int N, x;
-priority_queue<int> mxHeap;
+int heap[100'001];
+int sz = 1;
+
+void push(int n) {
+  int idx = sz++;
+  heap[idx] = n;
+
+  while (idx != 1) {
+    int par = idx / 2;
+
+    if (heap[par] >= heap[idx]) break;
+
+    swap(heap[par], heap[idx]);
+    idx = par;
+  }
+}
+
+bool empty(void) {
+  return sz == 1;
+}
+
+int top(void) {
+  return heap[1];
+}
+
+void pop() {
+  heap[1] = heap[--sz];
+  int idx = 1;
+
+  while (idx < sz) {
+    int chi = idx * 2;
+    
+    if (chi >= sz) {
+      break;
+    }
+
+    if (chi + 1 < sz && heap[chi + 1] > heap[chi]) {
+      chi = chi + 1;
+    }
+      
+    if (heap[idx] > heap[chi]) {
+      break;
+    }  
+   
+    swap(heap[idx], heap[chi]);
+    idx = chi;
+  }
+}
 
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
   cin >> N;
   while (N--) {
     cin >> x;
-    
+
     if (x != 0) { // 배열에 자연수 넣기
-      mxHeap.push(x);
+      push(x);
       continue;
     }
 
     //배열의 최대 출력, 제거
-    if (mxHeap.empty()) {
+    if (empty()) {
       cout << 0 << '\n';
       continue;
     }
 
-    cout << mxHeap.top() << '\n';
-    mxHeap.pop();
+    cout << top() << '\n';
+    pop();
   }
 }
