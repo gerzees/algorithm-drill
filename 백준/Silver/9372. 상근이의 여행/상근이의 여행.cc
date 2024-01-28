171b5f71ef5524b02a1374ab502730b7838da7b8
gerzees <49828022+gerzees@users.noreply.github.com>
2024-01-28 20:53:08 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,21 @@
+//20:44,20:52
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int T, N, M;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> T;
+  while (T--) {
+    cin >> N >> M;
+    int a, b;
+    while (M--) {
+      cin >> a >> b;
+    }
+    
+    cout << N - 1 << '\n';
+  }
+}
