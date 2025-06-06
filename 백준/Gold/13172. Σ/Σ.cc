0ecf37b7f0a195e3f81a94596a060c143a26c075
gerzees <49828022+gerzees@users.noreply.github.com>
2025-06-06 13:20:58 +0900

    [Gold IV] Title: Σ, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/13172. Σ/Σ.cc
+++ 백준/Gold/13172. Σ/Σ.cc
@@ -4,16 +4,6 @@ int M;
 unsigned long long p = 1'000'000'007ull;
 unsigned long long result;
 
-int get_gcd(unsigned long long a, unsigned long long b) {
-  unsigned long long r = a % b;
-
-  if (r == 0) {
-    return b;
-  }
-
-  return get_gcd(b, r);
-}
-
 int get_inverse(unsigned long long num, unsigned long long mod) {
   unsigned long long inverse = 1;
   unsigned long long exp = mod - 2;
@@ -37,15 +27,6 @@ int main() {
   for (int i = 0; i < M; ++i) {
     unsigned long long N, S;
     cin >> N >> S;
-    unsigned long long gcd = get_gcd(S, N);
-    N /= gcd;
-    S /= gcd;
-
-    if (S % N == 0) {
-      result += S / N;
-      continue;
-    }
-
     result += S * get_inverse(N, p);
     result %= p;
   }
