ba55c0feea8b82b775a26dab80ef2da7e46a75ca
gerzees <49828022+gerzees@users.noreply.github.com>
2025-06-09 16:25:13 +0900

    [Gold III] Title: 웜홀, Time: 588 ms, Memory: 3004 KB -BaekjoonHub

---
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef pair<int, int> pii;
+
+int TC;
+int N;
+int M;
+int W;
+int dist[501][501];
+vector<string> results;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> TC;
+  results.reserve(TC);
+
+  while (TC--) {
+    // 입력
+    cin >> N >> M >> W;
+
+    for (int i = 1; i <= N; ++i) {
+      fill(*(dist + i) + 1, *(dist + i) + N + 1, 1e5);
+    }
+
+    int S, E, T;
+
+    while (M--) {
+      cin >> S >> E >> T;
+
+      if (dist[S][E] > T) {
+        dist[S][E] = T;
+        dist[E][S] = T;
+      }
+    }
+
+    while (W--) {
+      cin >> S >> E >> T;
+
+      if (dist[S][E] > -T) {
+        dist[S][E] = -T;
+      }
+    }
+
+    // 처리
+    for (int i = 1; i <= N; ++i) {
+      for (int s = 1; s <= N; ++s) {
+        for (int e = 1; e <= N; ++e) {
+          dist[s][e] = min(dist[s][e], dist[s][i] + dist[i][e]);
+        }
+      }
+    }
+    bool yes = false;
+
+    for (int i = 1; i <= N; ++i) {
+
+      if (dist[i][i] < 0) {
+        yes = true;
+        break;
+      }
+    }
+
+    results.push_back(yes ? "YES" : "NO");
+  }
+
+  //출력
+  for (const string& result : results) {
+    cout << result << '\n';
+  }
+}
