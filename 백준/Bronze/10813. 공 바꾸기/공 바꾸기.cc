91cfe193a4fa0962799e0dbbd866571f1d1e2c98
gerzees <49828022+gerzees@users.noreply.github.com>
2025-06-27 11:29:41 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[101];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        arr[i] = i;
+    }
+
+    for (int cnt = 0; cnt < m; ++cnt)
+    {
+        int i, j;
+        cin >> i >> j;
+        swap(arr[i], arr[j]);
+    }
+
+    for (int i = 1; i <= n; ++i)
+    {
+        cout << arr[i] << ' ';
+    }
+}
