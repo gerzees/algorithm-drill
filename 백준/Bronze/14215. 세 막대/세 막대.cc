ac176939ea49658916975dc1138d737652c03f45
gerzees <49828022+gerzees@users.noreply.github.com>
2025-06-30 10:03:39 +0900

    [Bronze III] Title: 세 막대, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int sides[3];
+    cin >> sides[0] >> sides[1] >> sides[2];
+
+    sort(sides, sides + 3);
+
+    if (sides[2] < sides[0] + sides[1])
+    {
+        cout << sides[0] + sides[1] + sides[2];
+    }
+    else
+    {
+        cout << 2 * (sides[0] + sides[1]) - 1;
+    }
+}
